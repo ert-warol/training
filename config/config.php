b40bbf5a3725dc7063910d25d8827818483f9066
ert-warol <ertfediran@gmail.com>
2018-06-20 08:51:30 +0300

    first commit 20 june 2018

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ert
+ * Date: 05.08.2017
+ * Time: 15:32
+ */
+
+// A set of variables involved in the rendering
+
+$head = '<head>
+    <meta charset="UTF-8">
+    <title>Test script</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="bootstrap-3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap-3/css/bootstrap-theme.min.css">
+</head>';
+
