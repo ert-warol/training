8ff74080b2cf52c2be61917ff3f4abc9975463cd
ert-warol <ertfediran@gmail.com>
2018-07-08 10:51:20 +0300

    8 jule 2018 home 2

---
+++ test.php
@@ -0,0 +1,33 @@
+<?php
+
+$double = 2.3;
+$string = 'Hello';
+$boolean = TRUE;
+//$integer += 1;
+$result = $integer . $string;
+$main = "<h1> $result </h1>";
+$empty;
+$integer = 5;
+
+$integer += 2; //7
+$integer -= 2; //5
+$integer *= 2; //10
+$integer /= 2; //5
+$integer %= 2; 
+$integer .= ' test';
+$integer = $integer + $boolean;
+
+//$integer = gettype($integer);
+
+echo '<h1 style="text-align: center"> $integer = ' . $integer . '</h1>';
+
+
+
+
+
+
+
+
+
+
+ 
