b63638cb1b06bf5671b09842f7b5dd92f6e892e3
ert-warol <ertfediran@gmail.com>
2018-07-08 10:20:02 +0300

    8 jule 2018 home

--- test.php
+++ test.php
@@ -1,40 +1,33 @@
 <?php
 
-$integer = 2;
 $double = 2.3;
 $string = 'Hello';
 $boolean = TRUE;
-$integer += 1;
+//$integer += 1;
 $result = $integer . $string;
 $main = "<h1> $result </h1>";
 $empty;
+$integer = 5;
+
+$integer += 2; //7
+$integer -= 2; //5
+$integer *= 2; //10
+$integer /= 2; //5
+$integer %= 2; 
+$integer .= ' test';
+$integer = $integer + $boolean;
+
+//$integer = gettype($integer);
+
+echo '<h1 style="text-align: center"> $integer = ' . $integer . '</h1>';
 
 
-/*function sum($namberOne, $namberTwo) {    	
-	$result = $namberOne + $namberTwo;
-	$mainResult = $namberOne. ' + ' . $namberTwo . ' = ' . $result . '</br>';
-	return $mainResult;
-}
 
-$One = 5;
-$Two = 4;
 
-$mainResult = sum($One,$Two);*/
 
-//$validate = empty($empty);
 
-$validate = isset($integer);
-$mainResult = gettype($string);
 
-if($validate) {	
-	print('Переменная существует и не пустая ');
-} else {
-	print('Переменная является пустой или не существует ');
-}
 
-echo '</br>'.' Тип переменной равен - ' . $mainResult;
-
-var_dump($validate);
 
 
  
